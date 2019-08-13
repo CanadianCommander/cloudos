4526182be778e7ec98663655ab2d392d553df4ee
CanadianCommander <ben.benetti@shaw.ca>
2019-08-12 19:35:17 -0700

    Remove process specific information from the Program model.

--- db/migrate/20190811004228_create_programs.rb
+++ db/migrate/20190811004228_create_programs.rb
@@ -2,11 +2,8 @@ class CreatePrograms < ActiveRecord::Migration[6.0]
   def change
     create_table :programs do |t|
       t.string :name
-      t.string :container_id
       t.string :image_id
       t.string :icon_path
-      t.integer :memory_requirement
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,8 @@ ActiveRecord::Schema.define(version: 2019_08_11_004228) do
 
   create_table "programs", force: :cascade do |t|
     t.string "name"
-    t.string "container_id"
     t.string "image_id"
     t.string "icon_path"
-    t.integer "memory_requirement"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
